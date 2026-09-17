d77a7b45a5e995245a8cd73883a2e64025fea947
deokwoo97 <deokwoo97@gmail.com>
2026-09-17 15:09:41 +0900

    Update ollama_basic

---
+++ ollama_basic/2-1.ai_docent.py
@@ -0,0 +1,30 @@
+# 실습 : image to text
+from ollama import chat
+
+IMAGE_PATH = "imgs/img01.jpg"
+MODEL_NAME = "qwen3.5:9b"
+
+response = chat(
+    model=MODEL_NAME,
+    messages=[
+        {
+            "role": "user",
+            "content": """
+이 이미지를 한국어로 설명해줘.
+
+다음 형식으로 답변해줘.
+1. 전체 장면
+2. 주요 객체
+3. 배경
+4. 이미지에서 추론 가능한 상황
+""",
+            "images": [IMAGE_PATH],
+        }
+    ],
+    think=False,
+    stream=True,
+)
+
+# print(response.message.content)
+for chunk in response:
+	print(chunk.message.content, end="", flush=True)
